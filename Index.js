1eb533605bacd4f77c54476429103daee3f641a8
naoki <naokilamy@gmail.com>
2024-03-30 20:07:51 +0100

    delete old code

--- Index.js
+++
@@ -1,5 +0,0 @@
-window.addEventListener('load', function() {
-    var welcomeMessage = 'Welcome to my website!'; // Customize your welcome message here
-    alert(welcomeMessage); // Replace with the code to display the welcome message in your website
-  });
-  
